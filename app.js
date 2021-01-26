c0887a6d16dbdada6be9c68a884142c9fa24c5f1
Lucas <lucasaxelson2016@gmail.com>
2021-01-26 14:56:50 +0000

    JS Test

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+// Modal ================================
+
 const Modal = {
-    open() {
-        // Open Modal
-        // Add active class to Modal
-        const modalOverlay = document.querySelector('.modal-overlay').classList.add("active");
-    },
-    close() {
-        // Close Modal
-        // Remove active class from Modal
-        const modalOverlay = document.querySelector('.modal-overlay').classList.remove("active");
+    toggle() {
+        // Toggle Modal
+        // Add or Remove active class to Modal
+        const modalOverlay = document.querySelector('.modal-overlay').classList.toggle("active");
     }
 }
 
@@ -15,10 +12,51 @@ const newTransaction = document.querySelector('#newTransaction');
 const closeTransaction = document.querySelector('#closeTransaction');
 
 newTransaction.addEventListener('click', function () {
-    Modal.open();
+    Modal.toggle();
 });
 
 closeTransaction.addEventListener('click', function () {
-    Modal.close();
+    Modal.toggle();
 });
+
+// Transaction ================================
+
+// const tBody = document.querySelector('#tBody');
+// const form = document.querySelector('#modalForm');
+
+
+// form.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const inputDesc = document.querySelector('#description');
+//     const inputAmount = document.querySelector('#amount');
+//     const inputDate = document.querySelector('#date');
+
+//     createTransaction(inputDesc.value, inputAmount.value, inputDate.value);
+// });
+
+// const createTransaction = (inputDesc, inputAmount, inputDate) => {
+//     let row = document.createElement('tr');
+//     let desc = document.createElement('td');
+//     desc.classList.add('description');
+//     let amount = document.createElement('td');
+//     let date = document.createElement('td');
+//     date.classList.add('date');
+
+//     desc.value = inputDesc.value;
+//     amount.value = inputAmount.value;
+//     date.value = inputDate.value;
+
+//     // if (amount.value < 0) {
+//     //     amount.classList.add('expense');
+//     // } else {
+//     //     amount.classList.add('income');
+//     // };
+
+//     row.append(desc);
+//     row.append(amount);
+//     row.append(date);
+//     tBody.append(row);
+// };
+
 
