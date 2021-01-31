4d2fa2a030ffe6ee7c1bb5a8e07527b0220cffb1
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-01-31 15:22:41 +0000

    JS Update & Error Message
    Update to Modal & Transaction

--- app.js
+++ app.js
@@ -21,41 +21,52 @@ closeTransaction.addEventListener('click', function () {
     Modal.toggle()
 });
 
+const Errors = {
+    message() {
+    },
+    toggle() {
+        const errorMessage = document
+        .getElementById('errorMessage')
+        .classList.toggle("active")
+    }
+}
+
 // Transaction ================================
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Energy',
-        amount: -5000,
-        date: '23/01/2021',
-    },
-    {
-        id: 2,
-        description: 'Salary',
-        amount: 200000,
-        date: '24/01/2021',
-    },
-    {
-        id: 3,
-        description: 'Gas',
-        amount: -2500,
-        date: '27/01/2021',
-    },
-    {
-        id: 4,
-        description: 'Internet',
-        amount: -19000,
-        date: '30/01/2021',
-    },
-]
-
 const Transaction = {
-    all: transactions,
+    all: [
+        {
+            description: 'Energy',
+            amount: -5000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'Salary',
+            amount: 200000,
+            date: '24/01/2021',
+        },
+        {
+            description: 'Gas',
+            amount: -2500,
+            date: '27/01/2021',
+        },
+        {
+            description: 'Internet',
+            amount: -19000,
+            date: '30/01/2021',
+        },
+    ],
+
     add(transaction) {
         Transaction.all.push(transaction)
         App.reload()
     },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+        App.reload()
+    },
+
     income (){
         // Count Income
         let income = 0
@@ -66,6 +77,7 @@ const Transaction = {
         } )        
         return income
     },
+
     expenses (){
         // Count Expenses
         let expense = 0
@@ -76,6 +88,7 @@ const Transaction = {
         })
         return expense
     },
+
     total (){
         // Income - Expenses
         let total = this.income() + this.expenses()
@@ -85,11 +98,13 @@ const Transaction = {
 
 const DOM = {
     transactionsContainer: document.querySelector(`#data-table tbody`),
+
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
         DOM.transactionsContainer.appendChild(tr)
     },
+
     innerHTMLTransaction(transaction){
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
         const amount = Utils.formatCurrency(transaction.amount)
@@ -107,6 +122,7 @@ const DOM = {
 
         return html
     },
+
     updateBalance() {
         document
             .getElementById('incomeDisplay')
@@ -124,6 +140,11 @@ const DOM = {
 }
 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value) * 100
+
+        return value
+    },
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
         value = String(value).replace(/\D/g, "")
@@ -134,6 +155,64 @@ const Utils = {
         })
 
         return signal + value
+    },
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedData[1]}/${splittedDate[3]}`
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+
+        if (
+            description.trim() === "" || 
+            amount.trim() === "" || 
+            date.trim() === "" ) {
+                throw new Error("Please, make sure to fill all of the camps")
+            }
+    },
+
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date 
+        }
+    },
+
+    saveTransaction() {
+        Transaction.add(transaction)
+    },
+
+    submit(event) {
+        event.preventDefault()
+        try {
+            Form.validateFields()
+            const transaction = Form.formatValues()
+            saveTransaction(transaction)
+            Form.clearFields()
+        } 
+        catch (error) {
+            Errors.toggle()
+            errorMessage.innerHTML = error.message
+        }
     }
 }
 
